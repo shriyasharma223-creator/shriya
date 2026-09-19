7f1acfa9eb72f1aee9df018ec4dccc98e477b4f9
Shriya Sharma <shriyashrama223@gmail.com>
2026-09-19 10:42:46 +0530

    traffic light

---
+++ ar.py
@@ -0,0 +1,9 @@
+light=input("enter colour: ")
+if light=="red":
+    print("stop")
+elif light=="yellow":
+    print("wait")
+elif light=="green":
+    print ("go")
+else:
+    print("invalid coloour")       
